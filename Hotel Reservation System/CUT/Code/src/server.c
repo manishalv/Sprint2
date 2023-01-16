acd13d73b922043fda4b2e40f4aa9519adc1cfd1
SianGijo <66726322+SianGijo@users.noreply.github.com>
2023-01-16 10:12:18 +0530

    Update server.c

--- Hotel Reservation System/CUT/Code/src/server.c
+++ Hotel Reservation System/CUT/Code/src/server.c
@@ -363,6 +363,8 @@ void handle_book_room(int sockfd, msg_t *msg)
 
   	msg_t reply_msg;
   	int flag = 0;
+	
+	//Initializing counters for statistics
   	int single_room_count = 0;
 	int double_room_count = 0;
 	int deluxe_room_count = 0;
@@ -376,35 +378,40 @@ void handle_book_room(int sockfd, msg_t *msg)
       	printf("User name : %s \n", msg->msg_add2_data);
   	
 	
-  	//Reserving single room for a user 
+  	//Checking if the requested room type is single room 
   	if(strcmp(msg->msg_data,"single") == 0)
   	{
-      		  
+      		//Checking availability of each room  
   		for(int i = 0 ; i < 4 ; i++)
 		{
+			//Checking the dates if room is already booked
 			if(r1[i]->available == 1 )
 			{
+				//Comparing check in date of new user and check out of old user
 				int ret1 = checking_dates(r1[i]->check_out,msg->msg_add_data);
 				if(ret1 == 1)
                      		{
 					flag = 1;
+					//Updating new user details in the structure
                         		strcpy(r1[i]->uname,msg->msg_add2_data);     
                         		reply_msg.msg_int = r1[i]->room_no;
                         		strcpy(reply_msg.msg_data,msg->msg_add2_data); 
 					strcpy(r1[i]->check_in,msg->msg_add_data);
 					strcpy(r1[i]->check_out,msg->msg_add1_data);
+					//Sending reply message that room is available
 					reply_msg.msg_type = MSG_ROOM_AVAIL;
                                 
 					break;
 				}
 			}
 			
- 
+ 			//Checking if room is not booked
 			if(r1[i]->available == 0)
 			{
                      
 				flag = 1;
                         	r1[i]->available = 1;
+				//Updating new user details in the structure
 				strcpy(r1[i]->check_in,msg->msg_add_data);
 				strcpy(r1[i]->check_out,msg->msg_add1_data);
                         	strcpy(r1[i]->uname,msg->msg_add2_data);  
@@ -417,6 +424,7 @@ void handle_book_room(int sockfd, msg_t *msg)
 			}
                  
         	}
+		//If no rooms of type single room is available
 		if(flag == 0)
 			reply_msg.msg_type = MSG_ROOM_NOTAVAIL;
 
